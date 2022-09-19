af73664ab135effad2a52526df258d0a3c8c9991
Julien Zapata Duque <jzapata@live.nl>
2022-09-19 23:32:18 +0200

    feat: add movie db image component

---
+++ src/lib/components/movies/MovieDBImage.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let path: string;
+  // Only certain sizes are allowed https://developers.themoviedb.org/3/configuration/get-api-configuration
+  export let size: 342 | 500 = 342;
+
+  const relativePath = path.startsWith('/') ? path.slice(1) : path;
+</script>
+
+<img src={`https://image.tmdb.org/t/p/w${size}/${relativePath}`} alt="" />
