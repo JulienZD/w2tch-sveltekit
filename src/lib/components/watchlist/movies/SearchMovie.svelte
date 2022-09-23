defb9272e287a13661fc75e860f3f2634eaa53a6
Julien Zapata Duque <jzapata@live.nl>
2022-09-23 12:19:26 +0200

    fix: add theming to search component

--- src/lib/components/watchlist/movies/SearchMovie.svelte
+++ src/lib/components/watchlist/movies/SearchMovie.svelte
@@ -46,7 +46,7 @@
   };
 </script>
 
-<div class="form-control w-full">
+<div class="form-control w-full themed">
   <label class="label" for="movie">Movie</label>
   <Select
     id="movie"
@@ -61,3 +61,26 @@
     optionIdentifier="id"
   />
 </div>
+
+<style lang="postcss">
+  .themed {
+    --border: 1px solid currentColor;
+    --borderRadius: 4px;
+    --background: transparent;
+    --listBackground: theme(colors.base-100);
+    --listEmptyColor: theme(colors.base-content);
+    --placeholderColor: theme(colors.base-content);
+    --inputColor: theme(colors.base-content);
+    --itemColor: theme(colors.base-content);
+    --itemIsActiveColor: theme(colors.primary-content);
+    --itemIsActiveBG: theme(colors.primary);
+    --itemHoverColor: theme(colors.neutral-focus);
+    --itemHoverBG: theme(colors.base-300);
+  }
+
+  :global([data-theme='night']) .themed {
+    --listBackground: theme(colors.neutral);
+    --itemHoverColor: theme(colors.neutral-content);
+    --itemHoverBG: theme(colors.neutral-focus);
+  }
+</style>
