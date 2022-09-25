09d2a32c29cd0fa47ecb3cfb0a87c7b357810545
Julien Zapata Duque <jzapata@live.nl>
2022-09-25 17:02:08 +0200

    feat: optimistically update movie details

--- src/lib/stores/watchlistStore.ts
+++ src/lib/stores/watchlistStore.ts
@@ -1,7 +1,7 @@
 import type { Watchlist, WatchlistMovie } from '$lib/models';
-import type { WatchListAddMovie, WatchListPatchMovie } from '$lib/models/watchlist';
+import { zWatchListPatchMovie, type WatchListAddMovie, type WatchListPatchMovie } from '$lib/models/watchlist';
 import { api } from '$lib/util/api';
-import { writable } from 'svelte/store';
+import { get, writable } from 'svelte/store';
 
 const createWatchlistStore = () => {
   let watchlistId = '';
@@ -47,20 +47,37 @@ const createWatchlistStore = () => {
   const updateMovie = async (movieId: string, data: Partial<WatchListPatchMovie['input']>) => {
     if (!watchlistId) throw new Error('Missing watchlistId, was the store initialized?');
 
+    const valuePriorToUpdate = get(store).movies.find((m) => m.id === movieId);
+    if (!valuePriorToUpdate) {
+      return;
+    }
+
+    const updateMovieData = (newData: Partial<WatchlistMovie>) => {
+      update(({ movies, ...current }) => {
+        const movieIndex = movies.findIndex((m) => m.id === movieId);
+
+        movies[movieIndex] = { ...movies[movieIndex], ...newData };
+
+        return {
+          ...current,
+          movies,
+        };
+      });
+    };
+
+    const optimisticResult = zWatchListPatchMovie.safeParse(data);
+    if (!optimisticResult.success) return;
+
+    updateMovieData(optimisticResult.data);
+
     const result = await api.patch<WatchlistMovie>(`/api/watchlist/${watchlistId}/movies/${movieId}`, data);
+
     if (!result.success) {
+      updateMovieData(valuePriorToUpdate);
       return result;
     }
 
-    update(({ movies, ...current }) => {
-      const movieIndex = movies.findIndex((m) => m.id === result.data.id);
-      movies[movieIndex] = result.data;
-
-      return {
-        ...current,
-        movies,
-      };
-    });
+    updateMovieData(result.data);
 
     return result;
   };
