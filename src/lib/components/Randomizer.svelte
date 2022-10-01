6ebe0135001efc8a1416c1a7b83c5ab9f568a3f6
Julien Zapata Duque <jzapata@live.nl>
2022-10-01 21:02:29 +0200

    fix: gradually slow down randomizer, reset values on next go

--- src/lib/components/Randomizer.svelte
+++ src/lib/components/Randomizer.svelte
@@ -1,85 +1,102 @@
 <script lang="ts">
+  import { getCircularly, shuffle } from '$lib/util/array';
+  import { easeOutCirc } from '$lib/util/easing';
   import { range } from '$lib/util/range';
   import { wait } from '$lib/util/wait';
+  import { onMount } from 'svelte';
   import { fade, slide } from 'svelte/transition';
 
   export let items: string[] = [];
   export let shownOptionsCount = 7;
 
-  let selectedIndex = Math.floor(Math.random() * (items.length - 1 - 0 + 1) + 0);
   let finished = false;
   let isRandomizing = false;
 
-  $: n = items.length;
-  $: linearStepThrough = async () => {
+  let itemsToSelectFrom: string[] = items;
+  let currentlySelectedIndex: number = 0;
+
+  $: getRandomIndex = () => Math.floor(Math.random() * (items.length - 1 - 0 + 1) + 0);
+
+  $: resetRandomValues = () => {
+    itemsToSelectFrom = shuffle(items);
+    currentlySelectedIndex = getRandomIndex();
+  };
+
+  onMount(() => {
+    resetRandomValues();
+  });
+
+  $: randomize = async () => {
     if (isRandomizing) return;
+
+    resetRandomValues();
 
     finished = false;
     isRandomizing = true;
 
-    const getRandomIndex = (items: unknown[]) => Math.floor(Math.random() * (items.length - 1 - 0 + 1) + 0);
-    const offset = getRandomIndex(items);
+    const finalIndex = currentlySelectedIndex;
 
-    const iterations = items.length * 3;
+    const iterations = 20;
+    const easedIterationsCount = 10;
+    const easedMultiplier = 300;
+    const baseDelay = 100;
 
+    // Set the selected index such that we can iterate towards the final index
+    currentlySelectedIndex = finalIndex - (iterations + easedIterationsCount);
+
+    // Iterate at a fixed speed
     for (let i = 0; i < iterations; i++) {
-      selectedIndex = (((i + offset) % n) + n) % n;
+      await wait(baseDelay);
+      currentlySelectedIndex++;
+    }
 
-      const minDelay = 25;
-      const maxDelay = 1000;
-
-      const dynamicDelay = (i / items.length) * 150;
-
-      const delayInMs = Math.min(Math.max(minDelay, dynamicDelay), maxDelay);
-
-      await wait(delayInMs);
+    // Gradually slow down during the final few iterations
+    for (let i = 0; i < easedIterationsCount; i++) {
+      const eased = easeOutCirc(i, 0, 1, easedIterationsCount);
+      await wait(baseDelay + eased * easedMultiplier);
+      currentlySelectedIndex++;
     }
 
     isRandomizing = false;
     finished = true;
   };
 
-  $: _maxOptionsShown = Math.min(shownOptionsCount, n);
-  $: circularAccess = (index: number) => items[((index % n) + n) % n];
-
-  $: shownItems = range(_maxOptionsShown, -Math.floor(_maxOptionsShown / 2)).map((offset) => ({
-    value: circularAccess(selectedIndex + offset),
-    selected: offset === 0,
-  }));
+  let shownItems: { value: string; selected: boolean }[];
+  $: {
+    const maxOptionsShown = Math.min(shownOptionsCount, items.length);
+    shownItems = range(maxOptionsShown, -Math.floor(maxOptionsShown / 2)).map((offset) => ({
+      value: getCircularly(itemsToSelectFrom, currentlySelectedIndex + offset),
+      selected: offset === 0,
+    }));
+  }
 </script>
 
-{#if shownItems.length}
-  {#if finished}
-    <div class="h-full flex justify-center flex-col">
-      <span
-        in:fade
-        class="text-center whitespace-pre break-words w-full drop-shadow-2xl transition-all py-8 px-6! bg-primary-focus rounded-md text-xl md:text-3xl text-primary-content"
-      >
-        {circularAccess(selectedIndex)}
-      </span>
-    </div>
-    <div class="flex justify-center mt-4">
-      <button class="btn btn-primary btn-sm" disabled={isRandomizing} on:click={linearStepThrough}>Randomize</button>
-    </div>
-  {:else}
-    <div class="select-none w-full not-prose relative text-center" in:fade>
-      <ul class="min-h-[14rem]">
-        {#each shownItems as item (item.value)}
-          {@const isSelectedItem = item.selected}
-          <li
-            transition:slide|local={{ duration: 350 }}
-            class:font-bold={isSelectedItem}
-            class:text-lg={isSelectedItem}
-          >
-            <span class="transition-all">
-              {item.value}
-            </span>
-          </li>
-        {/each}
-      </ul>
-    </div>
-    <div class="flex justify-center mt-4">
-      <button class="btn btn-primary btn-sm" disabled={isRandomizing} on:click={linearStepThrough}>Randomize</button>
-    </div>
-  {/if}
+{#if finished}
+  <div class="h-full flex justify-center flex-col">
+    <span
+      in:fade
+      class="text-center whitespace-pre break-words w-full drop-shadow-2xl transition-all py-8 px-6! bg-primary-focus rounded-md text-xl md:text-3xl text-primary-content"
+    >
+      {getCircularly(itemsToSelectFrom, currentlySelectedIndex)}
+    </span>
+  </div>
+  <div class="flex justify-center mt-4">
+    <button class="btn btn-primary btn-sm" disabled={isRandomizing} on:click={randomize}>Randomize</button>
+  </div>
+{:else}
+  <div class="select-none w-full not-prose relative text-center" in:fade>
+    <ul class="min-h-[14rem]">
+      {#each shownItems as item (item.value)}
+        {@const isSelectedItem = item.selected}
+        <li transition:slide|local={{ duration: 350 }} class:font-bold={isSelectedItem} class:text-lg={isSelectedItem}>
+          <span class="transition-all">
+            {item.value}
+          </span>
+        </li>
+      {/each}
+    </ul>
+  </div>
+  <div class="flex justify-center mt-4">
+    <button class="btn btn-primary btn-sm" disabled={isRandomizing} on:click={randomize}>Randomize</button>
+  </div>
 {/if}
