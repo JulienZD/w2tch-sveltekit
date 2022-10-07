e192322cd3cc03b9c28b673e016c043b208980bd
Julien Zapata Duque <jzapata@live.nl>
2022-10-07 22:08:09 +0200

    fix: improve watchlist info responsiveness

--- src/routes/watchlist/[slug]/+page.svelte
+++ src/routes/watchlist/[slug]/+page.svelte
@@ -33,15 +33,21 @@
 
 <div class="prose max-w-full">
   <h1>{$watchlistStore.name}</h1>
-  <div class="flex text-sm gap-x-4">
-    <p>List by <span class="font-semibold">{$watchlistStore.owner?.name}</span></p>
-    <div class="flex items-center">
-      <p><Pluralize count={$watchlistStore.memberCount} word="member" /></p>
-      {#if $page.data?.isOwner}
-        <button on:click={openInviteModal} class="btn btn-ghost btn-sm"><PlusIcon /></button>
-      {/if}
+  <div class="flex text-sm gap-x-4 mb-4 items-end md:items-center justify-between md:justify-start">
+    <span>List by <span class="font-semibold">{$watchlistStore.owner?.name}</span></span>
+    <div class="my-0 flex flex-col gap-y-0 md:flex-row md:items-center">
+      <div class="flex items-center">
+        <span class="whitespace-nowrap"><Pluralize count={$watchlistStore.memberCount} word="member" /></span>
+        {#if $page.data?.isOwner}
+          <span data-tip="Create Invite" class="tooltip tooltip-left md:tooltip-top">
+            <button on:click={openInviteModal} aria-label="Create invite" class="mx-0.5 px-1 btn btn-ghost btn-sm"
+              ><PlusIcon size="20" /></button
+            >
+          </span>
+        {/if}
+      </div>
+      <span class="whitespace-nowrap"><Pluralize count={$watchlistStore.movieCount} word="movie" /></span>
     </div>
-    <p><Pluralize count={$watchlistStore.movieCount} word="movie" /></p>
   </div>
   <div class:flex={!showAddMovieForm} class={!randomizerMode ? 'justify-between' : 'justify-end'}>
     {#if !randomizerMode}
